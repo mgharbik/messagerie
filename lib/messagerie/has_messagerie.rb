9f29e9245397ee7ba73e9b02a3da503731f3a388
Mohammed Gharbi <simo163@hotmail.co.uk>
2015-10-30 11:05:56 +0100

    adding the instance method def conversations

--- lib/messagerie/has_messagerie.rb
+++ lib/messagerie/has_messagerie.rb
@@ -1,4 +1,23 @@
 module Messagerie
   module HasMessagerie
+    extend ActiveSupport::Concern
+
+    included do
+    end
+
+    module ClassMethods
+      def has_messagerie
+        include Messagerie::HasMessagerie::LocalInstanceMethods
+      end
+    end
+
+    module LocalInstanceMethods
+      def conversations
+        Conversation.where("(sender_id = :this_id AND sender_type = :this_type) OR
+            (receiver_id = :this_id AND receiver_type = :this_type)", this_id: id, this_type: self.class.name)
+      end
+    end
   end
 end
+
+ActiveRecord::Base.send(:include, Messagerie::HasMessagerie)
--- test/has_messagerie_test.rb
+++ test/has_messagerie_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class HasMessagerieTest < ActiveSupport::TestCase
+
+  def test_a_user_should_have_conversations
+    user = User.create!(email: "user@email.com")
+    assert_equal [], user.conversations
+  end
 end
